bbc7f1e8f59dbff4f2b4d1bfe1b0c7564560a8b3
harsh <harsh@harsh-Lenovo-IdeaPad-Y570.(none)>
2013-08-04 23:08:52 +0530

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thinker::Application.config.secret_key_base = 'a2b11bd71812119ddbd8200c6d5d359ebae1e1aab064a70a4c3fe769b76af6762ea4bcb0e8da555423b23fdf98d79359f2436a33cf5c2199a20bab577d37d1c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Thinker::Application.config.session_store :cookie_store, key: '_thinker_session'
